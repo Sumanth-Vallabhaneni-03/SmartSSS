6ffecd9a0f31fffc57e3b78e8361d0892d64f402
siri <22501a05j2@gmail.com>
2025-03-08 21:27:16 +0530

    commit deploy

--- server/index.js
+++ server/index.js
@@ -9,9 +9,12 @@ const Mentor = require("./models/mentors");
 
 
 const app = express();
-app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+const express = require("express");
+const cors = require("cors");
+
+
 
 
 const mongoURI = process.env.MONGO_URI;
@@ -55,6 +58,11 @@ app.post("/", async (req, res) => {  // 🔥 Changed from `/` to `/register`
   } catch (err) {
     return res.status(500).json({ error: "Failed to register user" });
   }
+
+});
+
+app.get("/health", (req, res) => {
+    res.send("✅ API connected to MongoDB and running!");
 });
 
 // ✅ FETCH MENTORS
