97d09e4a6a4320e720aa70201bb1d1be7d5ccaac
Sumanth-Vallabhaneni-03 <165824292+Sumanth-Vallabhaneni-03@users.noreply.github.com>
2025-03-09 15:23:40 +0530

    commit to deploy frontend

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const result = await axios.post("http://localhost:3000/login", { email, password });
+      const result = await axios.post("http://smart-bridge-backend.vercel.app/login", { email, password });
 
       if (result.data.token) {
         localStorage.setItem("token", result.data.token);
